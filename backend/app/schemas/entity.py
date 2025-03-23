f59944203cd989dacda3a40de4de5cc35812d5d3
Oleg <slepcov_oleg@mail.ru>
2025-03-23 20:48:40 +0700

    Change description of schemas

--- backend/app/schemas/entity.py
+++ backend/app/schemas/entity.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime
+from typing import Optional
 
 
 
@@ -8,20 +9,20 @@ class EntitySchema(BaseModel):
 
     id: int = Field(..., validation_alias='id')
     title: str = Field(..., validation_alias='title')
-    created_time: datetime | None = Field(..., validation_alias='createdTime')
+    created_time: Optional[datetime] = Field(..., validation_alias='createdTime')
     updated_time: datetime = Field(..., validation_alias='updatedTime')
     created_by: int = Field(..., validation_alias='createdBy')
-    assigned_by_id: int | None = Field(..., validation_alias='assignedById')
-    company_id: int | None = Field(..., validation_alias='companyId')
-    category_id: int | None = Field(..., validation_alias='categoryId')
+    assigned_by_id: Optional[int] = Field(..., validation_alias='assignedById')
+    company_id: Optional[int] = Field(..., validation_alias='companyId')
+    category_id: Optional[int] = Field(..., validation_alias='categoryId')
 
-    moved_time: datetime | None = Field(..., validation_alias='movedTime')
-    moved_by: int | None = Field(..., validation_alias='movedBy')
-    stage_id: str | None = Field(..., validation_alias='stageId')
-    previous_stage_id: str | None = Field(..., validation_alias='previousStageId')
-    opportunity: float | None = Field(..., validation_alias='opportunity')
-    product_id: int | None = Field(..., validation_alias='ufCrm21_1726480119')
-    product_id: int | None = Field(..., validation_alias='ufCrm21_1726637267')
+    moved_time: Optional[datetime] = Field(..., validation_alias='movedTime')
+    moved_by: Optional[int] = Field(..., validation_alias='movedBy')
+    stage_id: Optional[str] = Field(..., validation_alias='stageId')
+    previous_stage_id: Optional[str] = Field(..., validation_alias='previousStageId')
+    opportunity: Optional[float] = Field(..., validation_alias='opportunity')
+    product_id: Optional[int] = Field(..., validation_alias='ufCrm21_1726480119')
+    product_id: Optional[int] = Field(..., validation_alias='ufCrm21_1726637267')
 
 
 
--- backend/app/schemas/stage.py
+++ backend/app/schemas/stage.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, ConfigDict, Field
-
+from typing import Optional
 
 
 class StageSchema(BaseModel):
@@ -7,8 +7,8 @@ class StageSchema(BaseModel):
 
     id: int = Field(..., validation_alias='ID')
     entity_id: str = Field(..., validation_alias='ENTITY_ID')
-    category_id: int | None = Field(..., validation_alias='CATEGORY_ID')
+    category_id: Optional[int] = Field(..., validation_alias='CATEGORY_ID')
     status_id: str = Field(..., validation_alias='STATUS_ID')
     name: str = Field(..., validation_alias='NAME')
-    semantic: str | None = Field(..., validation_alias='SEMANTICS')
+    semantic: Optional[str] = Field(..., validation_alias='SEMANTICS')
 
