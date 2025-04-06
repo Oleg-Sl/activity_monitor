a91ca2c064b1dd357c83d9575a6bc41c02fdf343
Oleg <slepcov_oleg@mail.ru>
2025-04-06 12:25:06 +0700

    Fix

--- backend/app/bitrix24/factory.py
+++ backend/app/bitrix24/factory.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .bitrix_client_token import BitrixClient as TokenBitrixClient
-from .bitrix_client_webhook import BitrixClient as WebhookBitrixClient
+# from .bitrix_client_webhook import BitrixClient as WebhookBitrixClient
 from .bitrix_client import InterfaceBitrixClient
 from app.repositories.credentials import CredentialRepository
 
