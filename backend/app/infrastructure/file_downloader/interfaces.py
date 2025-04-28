28a9e3adf826693b5891d7c1254a81c87167525d
Oleg <slepcov_oleg@mail.ru>
2025-04-28 19:51:12 +0700

    Change interface of file loader

--- backend/app/infrastructure/file_downloader/interfaces.py
+++ backend/app/infrastructure/file_downloader/interfaces.py
@@ -1,10 +1,10 @@
-from typing import Protocol
+from typing import Protocol, Optional
 
 class IFileDownloader(Protocol):
     async def download_file(
         self, 
         url: str, 
-        filename: str | None = None,
+        filename: Optional[str] = None,
         compress_image: bool = False,
         quality: int = 85
     ) -> str:
