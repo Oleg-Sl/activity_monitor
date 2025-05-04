115f2e3a77633164c8d16d992a0a0404c84ec604
Oleg <slepcov_oleg@mail.ru>
2025-05-04 23:19:09 +0700

    Fix

--- backend/app/repositories/work_calendar_repository.py
+++ backend/app/repositories/work_calendar_repository.py
@@ -29,3 +29,12 @@ class WorkCalendarRepository(AbstractRepository):
         stmt = select(WorkCalendar)
         result = await self.session.execute(stmt)
         return result.scalars().all()
+
+    async def create_if_not_exist(self, data: dict) -> int:
+        stmt = select(WorkCalendar).where(WorkCalendar.date == data['date'])
+        result = await self.session.execute(stmt)
+        day = result.scalars().first()
+        if not day:
+            stmt = insert(WorkCalendar).values(**data).returning(WorkCalendar.id)
+            result = await self.session.execute(stmt)
+            return result.scalar_one()
