2487579faf4544409c6ae0ffbc6192597cd0c160
Oleg <slepcov_oleg@mail.ru>
2025-05-25 14:55:47 +0700

    Fix

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,4 @@ from .credential import Credentials
 from .stage_history import StageHistory
 from .production_order import ProductionOrder
 from .production_schedule import ProductionSchedule
+from .production_stage_history import ProductionStageHistory
