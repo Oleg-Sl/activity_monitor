d0791e56beb07e519aba6bd26b71e52867bd4474
Oleg <slepcov_oleg@mail.ru>
2025-12-27 20:41:33 +0700

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -5,7 +5,6 @@ from collections.abc import AsyncGenerator
 
 # engine = create_async_engine("sqlite+aiosqlite:///test.db", echo=True)
 engine = create_async_engine("postgresql+asyncpg://oleg:oleg@127.0.0.1:5432/oleg", echo=False)
-# engine = create_async_engine("postgresql+asyncpg://tamam:Kdwo28k7s02wkKh!@80.87.96.238:5432/monitoractivity", echo=False)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(DeclarativeBase):
